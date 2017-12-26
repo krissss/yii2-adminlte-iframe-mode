92d7eaf3c2b06d9da0f5a30841376e8070f07a37
kriss <462679766@qq.com>
2017-12-26 14:41:10 +0800

    fix: assets path

--- IframeAsset.php
+++ IframeAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class IframeAsset extends AssetBundle
 {
-    public $sourcePath = __DIR__ . '/static1.0';
+    public $sourcePath = __DIR__ . '/static/1.0';
 
     public $css = [
         'iframe-layout.css',
